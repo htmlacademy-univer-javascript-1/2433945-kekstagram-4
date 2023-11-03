afcd05eeefc326bd3cecba3841ed7d711d47b4cc
ashanastasia <61278838+ashanastasia@users.noreply.github.com>
2023-11-03 20:02:42 +0500

    task5.2

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+/*
 const  checkLength = (line, maxLength) => line.length <= maxLength;
 
 checkLength('проверяемая строка', 20);
@@ -10,3 +11,26 @@ function isPalindrome(str) {
 }
 
 isPalindrome('Город Ижевск свеж и дорог');
+*/
+function isMeetingWithinWorkingHours(startTime, endTime, meetingStart, meetingDuration) {
+  const workingStart = parseTime(startTime);
+  const workingEnd = parseTime(endTime);
+  const meetingStartParsed = parseTime(meetingStart);
+
+  const meetingEnd = addMinutes(meetingStartParsed, meetingDuration);
+
+  if (meetingStartParsed < workingStart || meetingEnd > workingEnd) {
+    return false;
+  }
+
+  return true;
+}
+
+function parseTime(time) {
+  const [hours, minutes] = time.split(':').map((part) => parseInt(part, 10));
+  return new Date(0, 0, 0, hours, minutes);
+}
+
+function addMinutes(date, minutes) {
+  return new Date(date.getTime() + minutes * 60000);
+}
